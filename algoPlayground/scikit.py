5db7bb01cd46c49b6c738334b4643aa566c7c57e
= <emmicourt@gmail.com>
2018-11-29 12:41:33 -0800

    continueing to play with sklearn

--- algoPlayground/scikit.py
+++ algoPlayground/scikit.py
@@ -1,28 +1,40 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
 from sklearn import svm, linear_model, metrics, KFold, cross_val_score, datasets
 from sklearn.neighbors import KNeighborsClassifier
 
-#scikit initializers 
+## how to load a dataset from csv file
+# f = open("filename.txt")
+## skip the header: 
+# f.readline()  
+# data = np.loadtxt(f)
+
+# scikit initializers
+# Use parameters to control 
 log = linear_model.LogisticRegression(solver='lbfgs', C=1e5, multi_class='multinomial')
 svc = svm.SVC(kernel='linear') 
 
-
-# fake datasets I am using until we have a good boii
+## how to load data from sklearn 
+## These are fake datasets I am using until we have a good boii
 iris = datasets.load_iris()
-iris_X = iris.data
-iris_y = iris.target
+iris_X = iris.data 		# this is how you load data 
+iris_y = iris.target	# this is how you load lables
 
+
+#####################
 # KNN on iris data
-np.random.seed(0)
+#####################
+
+# this preps the training and test data indicies so the algo knows which ones to 
+# train/test on
+np.random.seed(0)	#randomly splits training data 
 indices = np.random.permutation(len(iris_X))
 iris_X_train = iris_X[indices[:-10]]
 iris_y_train = iris_y[indices[:-10]]
 iris_X_test = iris_X[indices[-10:]]
 iris_y_test = iris_y[indices[-10:]]
 
-# sckikit initializer 
+# KKN initializer 
 # hey emily figure out what these mean thanks! 
 knn = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
            metric_params=None, n_neighbors=5, p=2,
